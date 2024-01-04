1292ede3ccfd992f3060d732f9c0e222a7320fc0
le thi phuong linh 16a1/0057.uneti.hn <ltplinh.dhkl16a1hn@sv.uneti.edu.vn>
2024-01-04 16:40:54 +0700

    Add files via upload

---
+++ BAITAP CHƯƠNG 1/bai1.py
@@ -0,0 +1,10 @@
+class hinhchunhat():
+    a=int(input('canh a='))
+    b=int(input('canh b='))
+    c=(a+b)*2
+    s=a*b
+    def display(self):
+        print('thong tin HCN gom 2 canh a ,b ,chu vi c, dien tich S')
+        print(self.a,self.b,self.c,self.s)
+nv1=hinhchunhat()
+nv1.display
